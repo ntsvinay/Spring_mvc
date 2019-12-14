b68d79e5716ae13e764ec9da0d75d9ca0c9b1623
vinay mohniya <ntsvinay@gmail.com>
2019-12-15 00:43:06 +0530

    update_15_dec_12_42

--- spring_mvc/src/main/java/spring_mvc/controller/HelloWord.java
+++ spring_mvc/src/main/java/spring_mvc/controller/HelloWord.java
@@ -4,11 +4,15 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import spring_mvc.model.HelloWorldModal;
 
 @Controller
+@ResponseBody
 public class HelloWord {
+	@RequestMapping("/helloWord")
 	public String handler(Model model) {
 		HelloWorldModal hwm = new HelloWorldModal();
 		hwm.setDateTime(LocalDateTime.now().toString());
